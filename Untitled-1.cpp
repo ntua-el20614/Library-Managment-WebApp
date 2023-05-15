96f39a5245860fa67955941f1142b96ac7dfbbb1
ChrisOPellouis <chrisstyles318@gmail.com>
2023-05-16 02:15:30 +0300

    changed the cout

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -7,6 +7,6 @@ int main(){
     cout<<"Hello World"<<endl;
     for(int i=0;i<5;i++)
         cout<<"ppeee"<<endl;
-        cout<<"ate tzai kani"<<endl;
+        cout<<"Ma eisai malakas re?"<<endl;
     return 0;
 }
