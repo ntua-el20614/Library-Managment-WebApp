1d1e414b1f6b3593b390653215b67d9e2a01f5ae
GeorgeOLoco <gpittakis@gmail.com>
2023-05-16 15:29:56 +0300

    esaasa to

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -8,7 +8,7 @@ int main(){
     for(int i=0;i<5;i++)
         cout<<"ppeee"<<endl;
         cout<<"Ma eisai malakas re?"<<endl;
-cout<<"irten o tiramolas"<<endl;
+        cout<<"irten o tiramolas"<<endl;
     cout<<"Test oti doulefkei pou to laptop mou - Chris"<<endl;
     return 0;
 }
