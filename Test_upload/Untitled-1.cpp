8a63e019721191aeca2287a4e5b8bb6dcc37b49a
ChrisOPellouis <chrisstyles318@gmail.com>
2023-05-17 18:29:23 +0300

    Lets begin mfs

---
+++ Test_upload/Untitled-1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    cout<<"Hello World"<<endl;
+    for(int i=0;i<5;i++)
+        cout<<"ppeee"<<endl;
+        cout<<"Ma eisai malakas re?"<<endl;
+        cout<<"irten o tiramolas"<<endl;
+    cout<<"Test oti doulefkei pou to laptop mou - Chris"<<endl;
+    return 0;
+}
