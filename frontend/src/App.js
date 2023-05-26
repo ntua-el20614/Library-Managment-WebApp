07653d8bc421acf70231b2c27b07c408ed873f00
ChrisOPellouis <chrisstyles318@gmail.com>
2023-05-27 01:21:36 +0300

    dont change me again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect,useState } from 'react'
+import Axios from "axios";
 
-function App() {
+const App = () => {
+  const [data,setData] = useState();
+
+  const getData = async()=>{
+    const response = await Axios.get("http://localhost:3305/all_users");
+    setData(response.data);
+  }
+
+
+  useEffect(()=>{
+    getData()
+  },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+    {data}
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
