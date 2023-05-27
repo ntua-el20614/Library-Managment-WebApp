fc0c5bb7786f0a3dae8cf4946df3211f65aa837d
GiorgosOLoco <gpittakis@gmail.com>
2023-05-27 14:58:38 +0300

    fixed

--- backend/api/all_books.js
+++ backend/api/all_books.js
@@ -1,62 +1,3 @@
-/*const express = require("express");
-const apiutils = require("../apiutils");
-const Parser = require("@json2csv/plainjs").Parser;
-const router = express.Router();
-
-router.get("/", async (req, res) => {
-  fetchBooks((err, results) => {
-    if (err) {
-      console.error('Error fetching Book data:', err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-
-    res.json(results);
-  });
-});
-
-router.get("/:id",async(req,res) =>{//kathe sxolio exei diki tou viviliothiki
-//ara to id tou sxoliou tha mas dinei tin lista me ta vivlia pou exei
-
-await apiutils.handleRequest(true,req,res,"Got all books!",
-async(conn) =>{//entoli gia na paw stin vasi na ferw ta vivlia
-    const return_books = await conn.query(
-
-        //pio katw des ta "book", "book.school_id" gia tin vasi mas
-
-        "SELECT * FROM book Where book.school_id=?",//analoga me tin vasi mas afto
-        [req.params.id]//edw an ktipisei error prepei na einai int,
-        //parseInt ""AAAANN xreiastei!""
-    );
-
-    json_res = []; //json file pou tha epistrepsume ta vivlia
-
-    for (elem of return_books){
-        json_res.push({//vazw 1-1 ola ta vivlia sto json
-
-            //onomasies swstes apo tin vasi mas:
-            isbn: elem.isbn,
-            schoolID: elem.school_id,
-            title: elem.title,
-        });
-    }
-
-    //to parakatw if gia na epistrepsume Comma-Separated-Values or Json
-    if (req.query.format == "csv") {
-        const opts = {
-          fields: ["isbn", "schoolID", "title"],
-        };
-        return new Parser(opts).parse(json_res);
-      } else {
-        return json_res;
-      }
-    }
-
-); 
-});
-
-module.exports = router;*/
-
 const pool = require('../dbconnector');
 
 // Function to fetch data from the database
@@ -71,7 +12,7 @@ const fetchData = (schoolId, callback) => {
     }
 
     // Execute a SQL query to fetch data
-    const query = 'SELECT * FROM book WHERE school_id = '+ schoolId;
+    const query = 'SELECT * FROM book b JOIN book_school bs ON b.isbn = bs.isbn JOIN school s ON bs.school_id = s.school_id WHERE bs.school_id =  '+ schoolId;
     connection.query(query, [schoolId], (err, results) => {
       // Release the connection back to the pool
       connection.release();
@@ -90,3 +31,4 @@ const fetchData = (schoolId, callback) => {
 
  
 module.exports = { fetchData };
+
