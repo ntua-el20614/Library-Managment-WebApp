6a063cb2b3a0581483f9a3543c1ec16e8b500c41
GiorgosOLoco <gpittakis@gmail.com>
2023-05-30 11:05:53 +0300

    allaksa tin select sto all_books gia na valli authors kai category. Tzai evala ta tzai sto front. Chris apla epintosa tes grammes 220-222 en sou allaksa tipote allon

--- backend/api/all_books.js
+++ backend/api/all_books.js
@@ -12,10 +12,18 @@ const fetchData = (schoolId, callback) => {
     }
 
     // Execute a SQL query to fetch data
-    const query = 'SELECT * FROM book b ' +
-    'JOIN book_school bs ON b.isbn = bs.isbn ' +
-    'JOIN school s ON bs.school_id = s.school_id ' +
-    'WHERE bs.school_id =  '+ schoolId;
+    const query = 
+    'SELECT book.isbn, book.title, book.publisher, book.pages, book.summary, book.book_language, book.keywords, book_school.copys, book_school.available_copys, ' +
+    "GROUP_CONCAT(DISTINCT author.author_name SEPARATOR ', ') AS authors, " +
+    "GROUP_CONCAT(DISTINCT category.category_name SEPARATOR ', ') AS categories " +
+'FROM book ' +
+'JOIN book_school ON book.isbn = book_school.isbn ' +
+'JOIN book_author ON book.isbn = book_author.isbn ' +
+'JOIN author ON book_author.author_id = author.author_id ' +
+'JOIN book_category ON book.isbn = book_category.isbn ' +
+'JOIN category ON book_category.category_id = category.category_id ' +
+'WHERE book_school.school_id = ' + schoolId +
+' GROUP BY book.isbn, book.title';
     connection.query(query, [schoolId], (err, results) => {
       // Release the connection back to the pool
       connection.release();
@@ -37,15 +45,9 @@ module.exports = { fetchData };
 
 //Select gia na epistrefi oti theli o chris sto front
 /*
-'SELECT book.isbn, book.title, book.publisher, book.pages, book.summary, book.book_language, book.keywords, book_school.copys, book_school.available_copys, ' +
-    'GROUP_CONCAT(DISTINCT author.author_name SEPARATOR ', ') AS authors, ' +
-    'GROUP_CONCAT(DISTINCT category.category_name SEPARATOR ', ') AS categories ' +
-'FROM book ' +
-'JOIN book_school ON book.isbn = book_school.isbn ' +
-'JOIN book_author ON book.isbn = book_author.isbn ' +
-'JOIN author ON book_author.author_id = author.author_id ' +
-'JOIN book_category ON book.isbn = book_category.isbn ' +
-'JOIN category ON book_category.category_id = category.category_id ' +
-'WHERE book_school.school_id = ' + schoolId +
-' GROUP BY book.isbn, book.title'
-*/
+
+
+'SELECT * FROM book b ' +
+    'JOIN book_school bs ON b.isbn = bs.isbn ' +
+    'JOIN school s ON bs.school_id = s.school_id ' +
+    'WHERE bs.school_id =  '+ schoolId;*/
