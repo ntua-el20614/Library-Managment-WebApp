b6ec631b8d88b1f91e8b85ba97af554c3d1227d6
GiorgosOLoco <gpittakis@gmail.com>
2023-05-30 14:45:11 +0300

    okay

---
+++ backend/api/updatebook.js
@@ -0,0 +1,33 @@
+const pool = require('../dbconnector')
+
+// Function to fetch data from the database
+
+const updateData = (approved, userid, callback) => {
+  // Get a connection from the pool
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error connecting to the database:', err);
+      callback(err, null);
+      return;
+    }
+
+    // Execute a SQL query to update data
+    const query =
+    
+    connection.query(query, [approved, userid], (err, result) => {
+      // Release the connection back to the pool
+      connection.release();
+
+      if (err) {
+        console.error('Error executing query:', err);
+        callback(err, null);
+        return;
+      }
+
+      // Pass the retrieved data to the callback function
+      callback(null, result);
+    });
+  });
+};
+
+module.exports = { updateData };
