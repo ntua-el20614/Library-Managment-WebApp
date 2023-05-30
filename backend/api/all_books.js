b0152b700f930af0cd1ac2db690964e6ee6b50a6
GiorgosOLoco <gpittakis@gmail.com>
2023-05-30 10:34:28 +0300

    nea select sto allbooks. ekama tin sxolio telia kato

--- backend/api/all_books.js
+++ backend/api/all_books.js
@@ -35,3 +35,17 @@ const fetchData = (schoolId, callback) => {
  
 module.exports = { fetchData };
 
+//Select gia na epistrefi oti theli o chris sto front
+/*
+'SELECT book.isbn, book.title, book.publisher, book.pages, book.summary, book.book_language, book.keywords, book_school.copys, book_school.available_copys, ' +
+    'GROUP_CONCAT(DISTINCT author.author_name SEPARATOR ', ') AS authors, ' +
+    'GROUP_CONCAT(DISTINCT category.category_name SEPARATOR ', ') AS categories ' +
+'FROM book ' +
+'JOIN book_school ON book.isbn = book_school.isbn ' +
+'JOIN book_author ON book.isbn = book_author.isbn ' +
+'JOIN author ON book_author.author_id = author.author_id ' +
+'JOIN book_category ON book.isbn = book_category.isbn ' +
+'JOIN category ON book_category.category_id = category.category_id ' +
+'WHERE book_school.school_id = ' + schoolId +
+' GROUP BY book.isbn, book.title'
+*/
