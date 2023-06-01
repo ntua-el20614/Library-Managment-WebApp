2ce1422aabaf293c97d9cdcbe08dd208d01c69cc
GiorgosOLoco <gpittakis@gmail.com>
2023-06-01 13:20:32 +0300

    extra check

--- backend/api/update_available_copys.js
+++ backend/api/update_available_copys.js
@@ -12,8 +12,8 @@ const updateData = (isbn, userid, copynum, callback) => {
         }
 
         // Execute a SQL query to update data
-        const query = "UPDATE book_school SET available_copys = available_copys + ? WHERE isbn = ? AND school_id = (SELECT school_id FROM handlers WHERE user_id = ?);";
-        connection.query(query, [copynum, isbn, userid], (err, result) => {
+        const query = "UPDATE book_school SET available_copys = available_copys + ? WHERE isbn = ? AND school_id = (SELECT school_id FROM handlers WHERE user_id = ?) AND (SELECT COUNT(*) FROM book_school WHERE isbn = ? AND school_id = (SELECT school_id FROM handlers WHERE user_id = ?)) > 0;";
+        connection.query(query, [copynum, isbn, userid, isbn, userid], (err, result) => {
             // Release the connection back to the pool
             connection.release();
 
