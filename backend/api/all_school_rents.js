ed1f81fcdc850cfcf39ce5a9e46066db81682d64
GiorgosOLoco <gpittakis@gmail.com>
2023-06-01 14:22:41 +0300

    added school id and user id in all_school_rents

--- backend/api/all_school_rents.js
+++ backend/api/all_school_rents.js
@@ -13,7 +13,7 @@ const fetchData = ([userid], callback) => {
 
         // Execute a SQL query to fetch data
         const query =
-        'SELECT DISTINCT r.rent_id, u.username, u.user_name, r.isbn, r.date_of_rent, r.returned, r.approved, b.title, bs.copys, bs.available_copys ' +
+        'SELECT DISTINCT r.rent_id, u.username, u.user_name, r.user_id, r.school_id, r.isbn, r.date_of_rent, r.returned, r.approved, b.title, bs.copys, bs.available_copys ' +
         'FROM rent r ' +
         'JOIN users u ON r.user_id = u.user_id ' +
         'JOIN book b ON r.isbn = b.isbn ' +
