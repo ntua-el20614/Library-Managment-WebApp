e4035227f50285528b664616d3fbfec436284847
GiorgosOLoco <gpittakis@gmail.com>
2023-06-01 14:53:44 +0300

    dcs

--- backend/api/all_school_reservations.js
+++ backend/api/all_school_reservations.js
@@ -14,7 +14,7 @@ const fetchData = ([userid], callback) => {
         // Execute a SQL query to fetch data
         const query =
         'SELECT r.reservation_id, u.username, u.user_name, r.user_id, r.school_id, r.isbn, r.date_of_reservation, r.approved, b.title, bs.copys, bs.available_copys ' +
-        'FROM reservations r ' +
+        'FROM reservation r ' +
         'JOIN users u ON r.user_id = u.user_id ' +
         'JOIN book b ON r.isbn = b.isbn ' +
         'JOIN book_school bs ON r.isbn = bs.isbn ' +
