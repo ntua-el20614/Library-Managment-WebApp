2ad16e3c0aa9af52c5927a6a283b81ec9bfb49e9
GiorgosOLoco <gpittakis@gmail.com>
2023-06-02 14:25:36 +0300

    ready to use the avg likert motherfucker

--- backend/api/avg_likert.js
+++ backend/api/avg_likert.js
@@ -1,4 +1,4 @@
-const pool = require('../dbconnector');
+/*const pool = require('../dbconnector');
 
 // Function to fetch data from the database
 
@@ -83,4 +83,97 @@ const fetchData = ([userid, username, categoryname], callback) => {
         });
     });
 };
+module.exports = { fetchData };*/
+
+const pool = require('../dbconnector');
+
+// Function to fetch data from the database
+const fetchData = ([userid, username, categoryname], callback) => {
+    // Get a connection from the pool
+    pool.getConnection((err, connection) => {
+        if (err) {
+            console.error('Error connecting to the database:', err);
+            callback(err, null);
+            return;
+        }
+
+        // Define the query based on the provided parameters
+        let query = '';
+        if (username !== '-1' && categoryname === '-1') {
+            // Only username provided
+            query =
+                'SELECT u.username, c.category_name, AVG(r.likert) AS average_likert ' +
+                'FROM review r ' +
+                'JOIN users u ON r.user_id = u.user_id ' +
+                'JOIN book_category bc ON r.isbn = bc.isbn ' +
+                'JOIN category c ON bc.category_id = c.category_id ' +
+                'JOIN book_school bs ON r.isbn = bs.isbn ' +
+                'JOIN handlers h ON bs.school_id = h.school_id ' +
+                'WHERE u.username = ? ' +
+                'AND h.user_id = ? ' +
+                'GROUP BY u.username, c.category_name';
+        } else if (username === '-1' && categoryname !== '-1') {
+            // Only categoryname provided
+            query =
+                'SELECT u.username, c.category_name, AVG(r.likert) AS average_likert ' +
+                'FROM review r ' +
+                'JOIN users u ON r.user_id = u.user_id ' +
+                'JOIN book_category bc ON r.isbn = bc.isbn ' +
+                'JOIN category c ON bc.category_id = c.category_id ' +
+                'JOIN book_school bs ON r.isbn = bs.isbn ' +
+                'JOIN handlers h ON bs.school_id = h.school_id ' +
+                'WHERE c.category_name = ? ' +
+                'AND h.user_id = ? ' +
+                'GROUP BY u.username, c.category_name';
+        } else if (username !== '-1' && categoryname !== '-1') {
+            // Both username and categoryname provided
+            query =
+                'SELECT u.username, c.category_name, AVG(r.likert) AS average_likert ' +
+                'FROM review r ' +
+                'JOIN users u ON r.user_id = u.user_id ' +
+                'JOIN book_category bc ON r.isbn = bc.isbn ' +
+                'JOIN category c ON bc.category_id = c.category_id ' +
+                'JOIN book_school bs ON r.isbn = bs.isbn ' +
+                'JOIN handlers h ON bs.school_id = h.school_id ' +
+                'WHERE u.username = ? ' +
+                'AND c.category_name = ? ' +
+                'AND h.user_id = ? ' +
+                'GROUP BY u.username, c.category_name';
+        } else if (username === '-1' && categoryname === '-1') {
+            // No parameters
+            query =
+                'SELECT u.username, c.category_name, AVG(r.likert) AS average_likert ' +
+                'FROM review r ' +
+                'JOIN users u ON r.user_id = u.user_id ' +
+                'JOIN book_category bc ON r.isbn = bc.isbn ' +
+                'JOIN category c ON bc.category_id = c.category_id ' +
+                'JOIN book_school bs ON r.isbn = bs.isbn ' +
+                'JOIN handlers h ON bs.school_id = h.school_id ' +
+                'WHERE h.user_id = ? ' +
+                'GROUP BY u.username, c.category_name';
+        }
+
+        // Prepare the query parameters
+        const params = [];
+        if (username !== '-1') params.push(username);
+        if (categoryname !== '-1') params.push(categoryname);
+        params.push(userid);
+
+        // Execute the query
+        connection.query(query, params, (err, results) => {
+            // Release the connection back to the pool
+            connection.release();
+
+            if (err) {
+                console.error('Error executing query:', err);
+                callback(err, null);
+                return;
+            }
+
+            // Pass the retrieved data to the callback function
+            callback(null, results);
+        });
+    });
+};
+
 module.exports = { fetchData };
