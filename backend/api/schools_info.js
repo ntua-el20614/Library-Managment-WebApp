1aff9b51914be5c10ea8ba7b3d9a56f89e7dbf52
GiorgosOLoco <gpittakis@gmail.com>
2023-06-04 17:55:27 +0300

    minore

--- backend/api/schools_info.js
+++ backend/api/schools_info.js
@@ -13,7 +13,7 @@ const fetchData = (callback) => {
     // Execute a SQL query to fetch data
     const query = 
     'SELECT ' +
-    's.school_name, ' +
+    's.school_id, s.school_name, ' +
     'u.user_name AS handler_name, ' +
     'COUNT(DISTINCT st.user_id) AS num_students, ' +
     'COUNT(DISTINCT t.user_id) AS num_teachers ' +
